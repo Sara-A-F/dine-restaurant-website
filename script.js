79f834d939a376eb2e87702ea81944c711c542f9
sferrer <saraferrer@gmail.com>
2021-03-30 14:29:00 +0200

    mobile version slider in process

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 // MANAGING THE SLIDER SECTION
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var titleSlide = document.getElementsByClassName("text-link");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < titleSlide.length; i++) {
+    titleSlide[i].className = titleSlide[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  titleSlide[slideIndex-1].className += " active";
+}
 
 // MANAGING THE DROPDOWN SELECTOR
 
